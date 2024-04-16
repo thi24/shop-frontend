6bf2fd0294f44d3299114c4dc34d02eeb6e3f2f2
Moritz Reindl <mor.reindl@gmail.com>
2024-04-16 18:54:20 +0200

    💄

--- Ticket-Shop/components/HeaderComponent.vue
+++ Ticket-Shop/components/HeaderComponent.vue
@@ -1,17 +1,20 @@
     <template>
         <header class="bg-white shadow-sm">
             <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 class="text-lm font-semibold leading-6 text-gray-900">Benevolo-Shop</h1>
+                <h1 class="text-lm font-semibold leading-6 text-white-900">Benevolo-Shop</h1>
             </div>
             <div class="p-4 pl-6 flex justify-center">
-                <div class="nav-bar">
-                    <div class="flex gap-4">
-                        <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="">Overview</NuxtLink>
-                        <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="">About Us</NuxtLink>
-                        <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="">Support</NuxtLink>
+                <form class="max-w-md mx-auto">   
+                    <label for="default-search" class="mb-2 text-sm font-medium text-white-900 sr-only dark:text-white">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-white-500 dark:text-white-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-white-900 border border-white-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-white-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Events" required />
                     </div>
-    
-                </div>
+                </form>
             </div>
         </header>
     </template>
