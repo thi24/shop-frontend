7065009976620ad1531b9dc48b3946fb40391cb5
RamonPH01 <rap4517@thi.de>
2024-04-16 16:59:59 +0200

    Resovling conflict-- Ramon

--- Ticket-Shop/app.vue
+++ Ticket-Shop/app.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
-<<<<<<< HEAD
     <HeaderComponent />
     <NuxtPage class="h-screen" />
     <FooterComponent class="object-bottom" />
-=======
-    <HeaderComponent/>
-    <NuxtPage class="h-screen"/>
-    <FooterComponent/>
->>>>>>> main
   </div>
 </template>
