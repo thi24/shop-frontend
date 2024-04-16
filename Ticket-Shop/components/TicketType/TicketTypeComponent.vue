c7caea23a87ec38d0cac9d84869bbfdcf7605ccc
RamonPH01 <rap4517@thi.de>
2024-04-16 15:49:03 +0200

    versuch Id hinzuzufügen

--- Ticket-Shop/components/TicketType/TicketTypeComponent.vue
+++ Ticket-Shop/components/TicketType/TicketTypeComponent.vue
@@ -3,9 +3,10 @@
         <div class="event__body">
             <label for="number-input" class="block mb-2 text-sm font-medium text-black">{{ ticketType.name }}</label>
             <label for="number-input" class="block mb-2 text-sm font-medium text-black">{{ ticketType.price }}</label>
-            <input type="number" min="1" max="999" value="1" class="w-16 py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+            <input  type="number" min="1" max="999" value="1" class="w-16 py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
         </div>
     </div>
+    
 </template>
 
 <script setup lang="ts">
@@ -13,7 +14,8 @@
 import type { TicketType } from '~/classes/TicketType';
 
 defineProps<{
-    ticketType: TicketType
+    ticketType: TicketType;
+    
 }>()
 </script>
 
--- Ticket-Shop/pages/buyTickets.vue
+++ Ticket-Shop/pages/buyTickets.vue
@@ -1,41 +1,46 @@
 <template>
 
-    <div class="w-full max-w-md flex h-screen">
+    <div class="w-full max-w-md ">
         <div class="mb-4">
-            <TicketTypeComponent v-for="ticketType in tickettypes" :ticketType="ticketType" :key="ticketType.id"></TicketTypeComponent>
+            <TicketTypeComponent v-for="(ticketType, index) in tickettypes" :ticketType="ticketType" :key="ticketType.id" :id="index">
+            </TicketTypeComponent>
         </div>
-        <button>Hier bezahlen</button>
+
     </div>
-
+    <button class="mt-4 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 transition-colors" @click=callFunction()>Hier
+        bezahlen</button>
 </template>
-  
+
 <script lang="ts" setup>
-    import { TicketType } from '~/classes/TicketType';
-    import TicketTypeComponent from '~/components/TicketType/TicketTypeComponent.vue';
+import { TicketType } from '~/classes/TicketType';
+import TicketTypeComponent from '~/components/TicketType/TicketTypeComponent.vue';
 
-    const tickettypes: Ref<TicketType[]> = ref(getTicketTypes());
+const tickettypes: Ref<TicketType[]> = ref(getTicketTypes());
 
-    function getTicketTypes(): TicketType[] {
-        const results: TicketType[] = [];
+function getTicketTypes(): TicketType[] {
+    const results: TicketType[] = [];
+    //hier in der Theorie aufruf ins Backend für das entsprechend angeclickte Event
+    const tickettype1 = new TicketType();
+    tickettype1.id = '01575dfb-eb8c-43bd-9182-376d2a7c5123';
+    tickettype1.name = "Freitag Ticket";
+    tickettype1.price = 19.99;
+    results.push(tickettype1);
 
-        const tickettype1 = new TicketType();
-        tickettype1.id = '01575dfb-eb8c-43bd-9182-376d2a7c5123';
-        tickettype1.name = "Freitag Ticket";
-        tickettype1.price = 19.99;
-        results.push(tickettype1);
+    const tickettype2 = new TicketType();
+    tickettype2.id = '01575dfb-eb8c-43bd-9182-376d2a7c5124';
+    tickettype2.name = "Samstag Ticket";
+    tickettype2.price = 24.99;
+    results.push(tickettype2);
 
-        const tickettype2 = new TicketType();
-        tickettype2.id = '01575dfb-eb8c-43bd-9182-376d2a7c5124';
-        tickettype2.name = "Samstag Ticket";
-        tickettype2.price = 24.99;
-        results.push(tickettype2);
+    const tickettype3 = new TicketType();
+    tickettype3.id = '01575dfb-eb8c-43bd-9182-376d2a7c5125';
+    tickettype3.name = "Wochenend Ticket";
+    tickettype3.price = 39.99;
+    results.push(tickettype3);
 
-        const tickettype3 = new TicketType();
-        tickettype3.id = '01575dfb-eb8c-43bd-9182-376d2a7c5125';
-        tickettype3.name = "Wochenend Ticket";
-        tickettype3.price = 39.99;
-        results.push(tickettype3);
-
-        return results;
-    }
+    return results;
+}
+function callFunction(){
+    alert("Wir lösen schonmal aus");
+}
 </script>
