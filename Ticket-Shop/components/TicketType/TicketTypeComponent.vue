75c73ce224e650c14f5f681a9c11b8bc727547ba
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-04-16 12:39:28 +0200

    dynamic tickettype loading
    added TicketType Class
    added Component for TicketType
    changed TicketType.vue

---
+++ Ticket-Shop/components/TicketType/TicketTypeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="p-6 flex-items-stretch">
+        <div class="event__body">
+            <label for="number-input" class="block mb-2 text-sm font-medium text-black">{{ ticketType.name }}</label>
+            <label for="number-input" class="block mb-2 text-sm font-medium text-black">{{ ticketType.price }}</label>
+            <input type="number" min="1" max="999" value="1" class="w-16 py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import type { TicketType } from '~/classes/TicketType';
+
+defineProps<{
+    ticketType: TicketType
+}>()
+</script>
+
--- Ticket-Shop/pages/buyTickets.vue
+++ Ticket-Shop/pages/buyTickets.vue
@@ -1,8 +1,40 @@
 <template>
 
+    <div class="w-full max-w-md flex h-screen">
+        <div class="mb-4">
+            <TicketTypeComponent v-for="ticketType in tickettypes" :ticketType="ticketType" :key="ticketType.id"></TicketTypeComponent>
+        </div>
+    </div>
+
 </template>
   
-<script>
+<script lang="ts" setup>
+    import { TicketType } from '~/classes/TicketType';
+    import TicketTypeComponent from '~/components/TicketType/TicketTypeComponent.vue';
 
+    const tickettypes: Ref<TicketType[]> = ref(getTicketTypes());
 
+    function getTicketTypes(): TicketType[] {
+        const results: TicketType[] = [];
+
+        const tickettype1 = new TicketType();
+        tickettype1.id = '01575dfb-eb8c-43bd-9182-376d2a7c5123';
+        tickettype1.name = "Freitag Ticket";
+        tickettype1.price = 19.99;
+        results.push(tickettype1);
+
+        const tickettype2 = new TicketType();
+        tickettype2.id = '01575dfb-eb8c-43bd-9182-376d2a7c5124';
+        tickettype2.name = "Samstag Ticket";
+        tickettype2.price = 24.99;
+        results.push(tickettype2);
+
+        const tickettype3 = new TicketType();
+        tickettype3.id = '01575dfb-eb8c-43bd-9182-376d2a7c5125';
+        tickettype3.name = "Wochenend Ticket";
+        tickettype3.price = 39.99;
+        results.push(tickettype3);
+
+        return results;
+    }
 </script>
