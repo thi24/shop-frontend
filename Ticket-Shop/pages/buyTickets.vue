8d8c2b585f4d0f855be58becc09cf0c19a62bd17
Klabautterr <htmlsite@skiff.com>
2024-04-23 18:13:09 +0200

    💄

--- Ticket-Shop/pages/buyTickets.vue
+++ Ticket-Shop/pages/buyTickets.vue
@@ -17,7 +17,7 @@
       
         <div class="flex justify-center mt-4 pb-4">
           <button
-            class="w-1/2 item-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded shadow"
+            class="relative z-0 h-12 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500"
             @click="logUserInputs"
           >
             Jetzt bezahlen
