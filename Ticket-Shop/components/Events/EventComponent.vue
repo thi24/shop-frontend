c7f072992d3b151ca559c89c7733198ff59b7d72
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-04-27 11:51:42 +0200

    :sparkles: Basic Backend Aufrufe
    Backend Aufruf für index und auf buyTickets.vue tickettypen

--- Ticket-Shop/components/Events/EventComponent.vue
+++ Ticket-Shop/components/Events/EventComponent.vue
@@ -1,31 +1,31 @@
 <template>
   <div
-    class=" bg-slate-100 flex flex-col items-center rounded-lg overflow-hidden shadow-xl  m-4 cursor-pointer"
+    class="bg-slate-100 flex flex-col items-center rounded-lg overflow-hidden shadow-xl m-4 cursor-pointer"
     @click="navigateToBuyTickets(event.id)"
   >
-    <img class="w-full h-48 " :src="event.thumbnail" alt="">
-    <div class="p-4 rounded-md hover:from-gray-300 hover:to-gray-200 active:shadow-none">
-      <h3 class="text-lg font-sans ">{{ event.eventName }}</h3>
-
+    <img class="w-full h-48" :src="event.thumbnail" alt="" />
+    <div
+      class="p-4 rounded-md hover:from-gray-300 hover:to-gray-200 active:shadow-none"
+    >
+      <h3 class="text-lg font-sans">{{ event.eventName }}</h3>
     </div>
-
-</div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-import type { Event } from '~/classes/Event';
-import { useEventStore } from '~/stores/eventIdStore';
+import { useRouter } from "vue-router";
+import type { Event } from "~/classes/Event";
+import { useEventStore } from "~/stores/eventIdStore";
 
 defineProps<{
-  event: Event
-}>()
+  event: Event;
+}>();
 
 const router = useRouter();
 const eventStore = useEventStore();
 
 function navigateToBuyTickets(eventId: string) {
   eventStore.setEventId(eventId);
-  router.push('/buyTickets');
+  router.push("/buyTickets");
 }
 </script>
--- Ticket-Shop/pages/buyTickets.vue
+++ Ticket-Shop/pages/buyTickets.vue
@@ -1,46 +1,47 @@
 <template>
-
   <div class="max-w-9xl mx-auto px-1 py-1 lg:px-3 lg:py-3">
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-0 sm:gap-2 hover:rounded-lg">
-    
-      <div class=" p-1 rounded-lg">
+    <div
+      class="grid grid-cols-1 lg:grid-cols-2 gap-0 sm:gap-2 hover:rounded-lg"
+    >
+      <div class="p-1 rounded-lg">
         <EventBuyComponent />
       </div>
-      
 
-      <div class=" p-5 lg:pt-10">
-        <div class="bg-slate-100 p-1 lg:rounded-lg rounded-lg lg:shadow-lg shadow-lg px-1 
-
-        ">
-        <div v-for="ticketType in tickettypes" :key="ticketType.id" class=" last:mb-0">
-          <TicketTypeComponent :ticketType="ticketType" />
-        </div>
-      
-        <div class="flex justify-center mt-4 pb-4">
-          <button
-            class="relative z-0 h-12 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500"
-            @click="logUserInputs"
+      <div class="p-5 lg:pt-10">
+        <div
+          class="bg-slate-100 p-1 lg:rounded-lg rounded-lg lg:shadow-lg shadow-lg px-1"
+        >
+          <div
+            v-for="ticketType in tickettypes"
+            :key="ticketType.id"
+            class="last:mb-0"
           >
-            Jetzt bezahlen
-          </button>
+            <TicketTypeComponent :ticketType="ticketType" />
+          </div>
+
+          <div class="flex justify-center mt-4 pb-4">
+            <button
+              class="relative z-0 h-12 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500"
+              @click="logUserInputs"
+            >
+              Jetzt bezahlen
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
 import { ref, type Ref } from "vue";
-import Popup from "../components/PaymentPopup/Popup.vue";
 
-import EventBuyComponent  from "~/components/Events/EventBuyComponent";
+import { useEventStore } from "~/stores/eventIdStore";
+
+import EventBuyComponent from "~/components/Events/EventBuyComponent";
 
 import { TicketType } from "~/classes/TicketType";
 import TicketTypeComponent from "~/components/TicketType/TicketTypeComponent.vue";
-
-const tickettypes: Ref<TicketType[]> = ref(getTicketTypes());
 
 const selectedTickets = ref<{ id: any; name: any; quantity: number }[]>([]);
 
@@ -48,43 +49,38 @@ const popupTriggers: Ref<{ buttonTrigger: boolean }> = ref({
   buttonTrigger: false,
 });
 
+const tickettypes = ref<TicketType[]>([]);
 
+const eventStore = useEventStore();
 
-function getTicketTypes(): TicketType[] {
-  const results: TicketType[] = [];
+onMounted(async () => {
+  const token = import.meta.env.VITE_AUTH_TOKEN;
+  var eventId = eventStore.eventId;
+  console.log(eventId);
 
-  const tickettype1 = new TicketType();
-  tickettype1.id = "01575dfb-eb8c-43bd-9182-376d2a7c5123";
-  tickettype1.name = "Freitag Ticket";
-  tickettype1.price = 19.99;
-  results.push(tickettype1);
-
-  const tickettype2 = new TicketType();
-  tickettype2.id = "01575dfb-eb8c-43bd-9182-376d2a7c5124";
-  tickettype2.name = "Samstag Ticket";
-  tickettype2.price = 24.99;
-  results.push(tickettype2);
-
-  const tickettype3 = new TicketType();
-  tickettype3.id = "01575dfb-eb8c-43bd-9182-376d2a7c5125";
-  tickettype3.name = "Wochenend Ticket";
-  tickettype3.price = 39.99;
-  results.push(tickettype3);
-
-  return results;
-}
+  try {
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    tickettypes.value = await $fetch(
+      "https://dev.benevolo.de/api/event-service/ticket-types?eventId=383f700f-5449-4e40-b509-bee0b5d139d6",
+      options
+    );
+  } catch (error) {
+    console.error("Failed to load events:", error);
+  }
+});
 
 function logUserInputs() {
   selectedTickets.value = [];
 
-
   tickettypes.value.forEach((ticketType) => {
-   
     const inputElement = document.querySelector(
       `#number-input-${ticketType.id}`
     ) as HTMLInputElement;
     if (inputElement && parseInt(inputElement.value) != 0) {
-      
       selectedTickets.value.push({
         id: ticketType.id,
         name: ticketType.name,
@@ -102,5 +98,4 @@ function logUserInputs() {
 function togglePopup() {
   popupTriggers.value.buttonTrigger = !popupTriggers.value.buttonTrigger;
 }
-
 </script>
