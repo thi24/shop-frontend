75c7a57fa4f32c092915f9eaba40aea3541a1dad
Linus <lib2407@thi.de>
2024-05-14 10:58:32 +0200

    loading animation & bilder laden logisch besser verteilt -l 🚧

--- Ticket-Shop/stores/eventIdStore.js
+++ Ticket-Shop/stores/eventIdStore.js
@@ -4,16 +4,13 @@ import { defineStore } from 'pinia';
 export const useEventStore = defineStore('event', {
     state: () => ({
       eventId: '',
-      thumbnail: "",
     }),
     persist: true,
     actions: {
       setEventId(id) {
         this.eventId = id;
       },
-      setEventThumbnail(thumbnail) {
-        this.thumbnail = thumbnail
-      }
+
     },  
   });
 
