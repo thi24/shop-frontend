be65f85e88844462f0a1e6e34b0157bbbc021c68
Ramon Perez Heide <ramon@MacBook-Pro-von-Ramon.local>
2024-05-18 17:51:20 +0200

    klein Anpassung error page -Ramon

--- Ticket-Shop/pages/error.vue
+++ Ticket-Shop/pages/error.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <h1>Da ist was schief gelaufen :(</h1>
-    <p> Test</p>  </div>
+    <div>
+        <h1>Da ist was schief gelaufen :(</h1>
+    </div>
+    <div v-if="processError">
+        <p>Text für wenn Prozess fehschlägt</p>
+        <p></p>
+    </div>
+    <div v-if="paymentError">
+        <p>Text für wenn Zahlung fehschlägt</p>
+        <p></p>
+    </div>
 </template>
 
 <script lang="ts" setup>
