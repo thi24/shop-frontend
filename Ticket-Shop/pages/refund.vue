d83f8c8a16d46ce9108fec6d8b6559b0c04f35b0
RamonPH01 <rap4517@thi.de>
2024-05-21 12:39:45 +0200

    Styling 💄 -Ramon

--- Ticket-Shop/pages/refund.vue
+++ Ticket-Shop/pages/refund.vue
@@ -5,7 +5,12 @@
         <ul>
             <li v-for="ticket in tickets" :key="ticket.id">
                 <span>{{ ticket.name }}</span>
-                <button @click="refundTicket(ticket.id)">Rückerstatten</button>
+                <button
+                    class="relative z-0 h-12 m-5 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500"
+                    @click="refundTicket(ticket.id)"
+                >
+                    Rückerstatten
+                </button>
             </li>
         </ul>
     </div>
