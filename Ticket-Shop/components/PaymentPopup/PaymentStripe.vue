2fab54596f6f36fc6c53fc40e19b144a8b05f40e
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-05-24 17:16:26 +0200

    Pfeil größer

--- Ticket-Shop/components/PaymentPopup/PaymentStripe.vue
+++ Ticket-Shop/components/PaymentPopup/PaymentStripe.vue
@@ -159,8 +159,8 @@ function handleLoadingArrow() {
           <div v-if="arrow && loading" class="ml-3">
             <svg
               aria-hidden="true"
-              class="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
+              class="inline w-7 h-7 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 100"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
