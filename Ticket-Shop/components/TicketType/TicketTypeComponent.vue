ffef584c2cf3f508280013e6bf61f43a3a15645c
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-06-04 16:01:59 +0200

    :sparkles: TicketTypes als Component

--- Ticket-Shop/components/TicketType/TicketTypeComponent.vue
+++ Ticket-Shop/components/TicketType/TicketTypeComponent.vue
@@ -1,23 +1,43 @@
 <template>
-
-<template>
-  <div class="p-3 m-3 flex bg-white border-b">
-    <label :for="'number-input-' + ticketType.id" class="flex-1">{{ ticketType.name }}</label>
-    <label :for="'number-input-' + ticketType.id" class="flex-1">{{ ticketType.price }}€</label>
-    <input :id="'number-input-' + ticketType.id" type="number" min="0" max="999" v-model="ticketQuantity" class="p-1 rounded-lg focus:outline-none" />
+  <div
+    class="relative bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none !m-0 p-6"
+  >
+    <h5
+      class="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 capitalize"
+    >
+      Tickets
+    </h5>
+  </div>
+  <div class="p-6 border-t border-blue-gray-50">
+    <div
+      v-for="ticketType in tickettypes"
+      :key="ticketType.id"
+      class="p-3 m-3 flex bg-white border-b"
+    >
+      <label :for="'number-input-' + ticketType.id" class="flex-1">{{
+        ticketType.name
+      }}</label>
+      <label :for="'number-input-' + ticketType.id" class="flex-1"
+        >{{ ticketType.price }}€</label
+      >
+      <input
+        :id="'number-input-' + ticketType.id"
+        type="text"
+        min="0"
+        max="999"
+        placeholder="0"
+        class="p-1 rounded-lg focus:outline-none w-11 placeholder-gray-400 border-gray-300 border [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+      />
+    </div>
   </div>
 </template>
 
-</template>
-
 <script setup lang="ts">
-
-
-defineProps<{
-  ticketType: TicketType;
-}>();
-
 import { TicketType } from "~/classes/TicketType";
 
+const props = defineProps<{
+  tickettypes: TicketType[];
+}>();
 
+const { tickettypes } = toRefs(props);
 </script>
--- Ticket-Shop/pages/buyTickets.vue
+++ Ticket-Shop/pages/buyTickets.vue
@@ -14,50 +14,18 @@
       class="w-full rounded-lg sm:w-2/5 md:w-2/5 lg:w-2/5 xl:w-2/5 2xl:w-2/5"
     >
       <div
+        v-if="tickettypes"
         class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md border border-blue-gray-100 mb-4 mt-1 mr-4 ml-4 sm:mt-4 sm:mr-0 sm:ml-0 md:mt-4 md:mr-0 md:ml-2 lg:ml-8 lg:mr-0 xl:mr-10 xl:ml-10 2xl:mr-5 2xl:ml-20"
       >
-        <div
-          class="relative bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none !m-0 p-6"
-        >
-          <h5
-            class="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 capitalize"
-          >
-            Tickets
-          </h5>
-        </div>
-        <div class="p-6 border-t border-blue-gray-50">
-          <div
-            v-for="ticketType in tickettypes"
-            :key="ticketType.id"
-            class="p-3 m-3 flex bg-white border-b"
-          >
-            <label :for="'number-input-' + ticketType.id" class="flex-1">{{
-              ticketType.name
-            }}</label>
-            <label :for="'number-input-' + ticketType.id" class="flex-1"
-              >{{ ticketType.price }}€</label
-            >
-            <input
-              :id="'number-input-' + ticketType.id"
-              type="text"
-              min="0"
-              max="999"
-              placeholder="0"
-              class="p-1 rounded-lg focus:outline-none w-11 placeholder-gray-400 border-gray-300 border [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-            />
-          </div>
-          <!--<div v-for="tickettype in tickettypes">
-            <TicketTypeComponent :tickettype="tickettype"></TicketTypeComponent>
-          </div>-->
+        <TicketTypeComponent :tickettypes="tickettypes" />
 
-          <div class="flex justify-center">
-            <button
-              class="w-1/2 align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg border border-blue-500 text-blue-500 hover:opacity-75 focus:ring focus:ring-blue-200 active:opacity-[0.85]"
-              @click="handlePayment"
-            >
-              Jetzt bezahlen
-            </button>
-          </div>
+        <div class="flex justify-center">
+          <button
+            class="mb-8 w-1/2 align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg border border-blue-500 text-blue-500 hover:opacity-75 focus:ring focus:ring-blue-200 active:opacity-[0.85]"
+            @click="handlePayment"
+          >
+            Jetzt bezahlen
+          </button>
         </div>
       </div>
       <PaymentPopup ref="paymentPopup" :selectedTickets="selectedTickets" />
