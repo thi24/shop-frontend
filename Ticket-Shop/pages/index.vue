55aaf94b65179eb672b424e1ad7a80bc6f89f46c
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-06-17 10:04:12 +0200

    :sparkles: Text erscheint, wenn es keine Suchergebnisse beim Events Fetchen gibt

--- Ticket-Shop/pages/index.vue
+++ Ticket-Shop/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="mx-auto">
+    <div class="flex justify-center items-center">
+      <p
+        id="no-results"
+        class="flex justify-center text-gray-600 my-10"
+        style="display: none"
+      >
+        Keine Suchergebnisse
+      </p>
+    </div>
     <div
       class="pt-4 px-4 hover:rounded-lg rounded-3xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-6 gap-4"
     >
@@ -23,9 +32,19 @@ onMounted(async () => {
     const eventName = searchStore.eventName;
     writeStoreInInput(eventName);
 
-    events.value = await (eventName == null || eventName == ""
-      ? fetchEvents()
-      : fetchEventsByEventName(eventName));
+    if (eventName == null || eventName == "") {
+      try {
+        events.value = await fetchEvents();
+      } catch (error) {}
+      document.getElementById("no-results").style.display =
+        events.value.length === 0 ? "block" : "none";
+    } else {
+      try {
+        events.value = await fetchEventsByEventName(eventName);
+      } catch (error) {}
+      document.getElementById("no-results").style.display =
+        events.value.length === 0 ? "block" : "none";
+    }
   } catch (error) {
     console.error("Failed to load events:", error);
   }
