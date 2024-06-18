3084c75f8a4afa4f84269b0297b982e17430746a
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-06-18 17:45:31 +0200

    :sparkles: Link zum Adminbereich

--- Ticket-Shop/components/Footer/FooterComponent.vue
+++ Ticket-Shop/components/Footer/FooterComponent.vue
@@ -1,28 +1,33 @@
 <template>
-    <footer class="bg-stone-100 border-t-2 border-stone-200">
-        <div
-            class="flex justify-center items-center w-full p-4 mx-auto max-w-screen-xl"
+  <footer class="bg-stone-100 border-t-2 border-stone-200 p-4">
+    <div class="container mx-auto flex flex-col md:flex-row items-center">
+      <div
+        class="flex flex-1 justify-center md:justify-center space-x-8 text-gray-500 mb-4 md:mb-0"
+      >
+        <a href="#" class="hover:text-black">About</a>
+        <a href="#" class="hover:text-black">Kontakt</a>
+        <a href="#" class="hover:text-black">Lizenz</a>
+        <a href="#" class="hover:text-black">Privatsphäre</a>
+      </div>
+      <div class="flex justify-center md:justify-end w-full md:w-auto">
+        <a
+          href="https://dev.benevolo.de"
+          class="bg-stone-200 hover:bg-stone-300 text-gray-500 py-2 px-4 rounded"
         >
-            <ul
-                class="flex flex-wrap justify-center items-center text-sm font-medium text-gray-500"
-            >
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6"
-                        >Privacy Policy</a
-                    >
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6"
-                        >Licensing</a
-                    >
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </footer>
+          Adminbereich
+        </a>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<style scoped>
+@media (min-width: 768px) {
+  .container {
+    flex-direction: row;
+  }
+  .container .mb-4 {
+    margin-bottom: 0;
+  }
+}
+</style>
