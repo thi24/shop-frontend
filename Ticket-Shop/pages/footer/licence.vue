2eb02cc7d5ea4ab0d9e6a82590cda7e112ea47e1
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-06-21 15:36:52 +0200

    :sparkles: Standard Seiten für Footer Links

---
+++ Ticket-Shop/pages/footer/licence.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-10 bg-white p-4 rounded-xl shadow-md p-6 rounded-lg shadow-lg">
+    <h2 class="text-2xl font-bold mb-4 text-gray-900">Lizenz</h2>
+    <p class="text-gray-600">Informationen über die Lizenzbedingungen.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Lizenz",
+};
+</script>
+
+<style>
+/* Styles for Lizenz */
+</style>
---
+++ Ticket-Shop/pages/footer/privacy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="m-10 bg-white p-4 rounded-xl shadow-md p-6 rounded-lg shadow-lg">
+    <h2 class="text-2xl font-bold mb-4 text-gray-900">Privatsphäre</h2>
+    <p class="text-gray-600">
+      Informationen über die Privatsphäre und Datenschutzrichtlinien.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Privacy",
+};
+</script>
