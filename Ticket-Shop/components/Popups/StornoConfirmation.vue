16f9ba30f568d346bde08611090888dabe180882
Linus <lib2407@thi.de>
2024-06-22 13:04:48 +0200

    styling stronoComponent -l

--- Ticket-Shop/components/Popups/StornoConfirmation.vue
+++ Ticket-Shop/components/Popups/StornoConfirmation.vue
@@ -6,36 +6,41 @@
     <div
       class="mt-[20vh] bg-white p-6 rounded-2xl shadow-lg  sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3"
     >
-      <h2 class="text-lg font-bold mb-4 text-center text-blue-500">Bestätigung</h2>
+      <h2 class="text-2xl font-bold mb-4 text-center text-blue-600">Bestätigung</h2>
       <p class="mb-4">
         Sind Sie sicher, dass Sie die folgenden Tickets stornieren möchten?
       </p>
-      <ul class="my-5">
+      <ul class="my-5 w-full">
         <li
           v-for="ticket in selectedTicketsDetails"
           :key="ticket.id"
           class="mb-2 text-center"
         >
-          {{ ticket.bookingItem?.ticketType?.name }} -
-          {{
-            ticket.price ? (ticket.price / 100).toLocaleString("de-DE", {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            }) : "0,00"
-          }}
-          €
+          <span class="font-semibold">{{ ticket.bookingItem?.ticketType?.name }}</span>
+          <span class="mx-2 md:mx-4">
+            -
+          </span>
+          <span class="font-normal">
+            {{
+              ticket.price ? (ticket.price / 100).toLocaleString("de-DE", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              }) : "0,00"
+            }}
+            €
+          </span>
         </li>
       </ul>
-      <div class="flex justify-end gap-4">
+      <div class="flex justify-center gap-20 py-4 items-center">
         <button
           @click="cancel"
-          class="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+          class="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 transition"
         >
           Abbrechen
         </button>
         <button
           @click="confirm"
-          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
         >
           Bestätigen
         </button>
@@ -62,4 +67,7 @@ const cancel = () => {
   emit("cancel");
 };
 </script>
+
+
+
 
