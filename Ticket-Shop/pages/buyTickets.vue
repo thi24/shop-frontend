08d4b4c85dd0369744d945b37564b290c8a12316
beckjohannes <90029916+beckjohannes@users.noreply.github.com>
2024-06-25 18:49:19 +0200

    :lipstick: svg tags ausgelagert & imports sortiert & Ordernamen klein & Bilder in assets verschoben

--- Ticket-Shop/pages/buyTickets.vue
+++ Ticket-Shop/pages/buyTickets.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
-    <AlertComponent v-if="errorMessage" :message="errorMessage" @close="errorMessage = null" />
+    <AlertComponent
+      v-if="errorMessage"
+      :message="errorMessage"
+      @close="errorMessage = null"
+    />
     <div class="flex flex-wrap">
       <!-- ab hier EventBuyComponent -->
       <div
-        class="w-full rounded-lg sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 p-1 mb-1 mt-2 mr-2 ml-2 sm:mr-6 sm:ml-5 md:mr-2 md:ml-8 lg:ml-7 lg:mr-8 xl:mr-5 xl:ml-20 2xl:mr-10 2xl:ml-30">
+        class="w-full rounded-lg sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 p-1 mb-1 mt-2 mr-2 ml-2 sm:mr-6 sm:ml-5 md:mr-2 md:ml-8 lg:ml-7 lg:mr-8 xl:mr-5 xl:ml-20 2xl:mr-10 2xl:ml-30"
+      >
         <div v-if="event" class="p-1 rounded-lg">
           <EventBuyComponent :event="event" />
         </div>
       </div>
       <!-- ab hier Ticket auswahl Component -->
-      <div class="w-full rounded-lg sm:w-2/5 md:w-2/5 lg:w-2/5 xl:w-2/5 2xl:w-2/5">
-        <div v-if="tickettypes"
-          class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md mb-4 mt-1 mr-4 ml-4 sm:mt-4 sm:mr-0 sm:ml-0 md:mt-4 md:mr-0 md:ml-2 lg:ml-8 lg:mr-0 xl:mr-10 xl:ml-10 2xl:mr-5 2xl:ml-20">
+      <div
+        class="w-full rounded-lg sm:w-2/5 md:w-2/5 lg:w-2/5 xl:w-2/5 2xl:w-2/5"
+      >
+        <div
+          v-if="tickettypes"
+          class="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md mb-4 mt-1 mr-4 ml-4 sm:mt-4 sm:mr-0 sm:ml-0 md:mt-4 md:mr-0 md:ml-2 lg:ml-8 lg:mr-0 xl:mr-10 xl:ml-10 2xl:mr-5 2xl:ml-20"
+        >
           <TicketTypeComponent :tickettypes="tickettypes" />
-          <div class="flex justify-center my-10" id="no-tickets" style="display: none">
-            <p class="flex justify-center mb-10">Aktuell keine Tickets verfügbar</p>
+          <div
+            class="flex justify-center my-10"
+            id="no-tickets"
+            style="display: none"
+          >
+            <p class="flex justify-center mb-10">
+              Aktuell keine Tickets verfügbar
+            </p>
           </div>
-          <div class="flex justify-center" >
-            <button v-if="hasTicketTypes"
+          <div class="flex justify-center">
+            <button
+              v-if="hasTicketTypes"
               class="mb-8 mt-2 w-1/2 max-w-xs align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg border border-blue-500 text-blue-500 hover:opacity-75 focus:ring focus:ring-blue-200 active:opacity-[0.85]"
-              @click="handlePayment">
-              Jetzt bezahlen    
+              @click="handlePayment"
+            >
+              Jetzt bezahlen
             </button>
           </div>
         </div>
@@ -34,17 +51,17 @@
 <script lang="ts" setup>
 import { ref, type Ref, computed } from "vue";
 import { useEventStore } from "~/stores/eventIdStore";
-import EventBuyComponent from "~/components/Events/EventBuyComponent.vue";
-import TicketTypeComponent from "~/components/TicketType/TicketTypeComponent.vue";
-import AlertComponent from "~/components/Alerts/Alert1Component.vue";
-import PaymentPopup from "~/components/Popups/PaymentPopup.vue";
 import { Event } from "~/classes/Event";
 import { TicketType } from "~/classes/TicketType";
-
 import {
   fetchTicketTypesByEventId,
   fetchEventById,
 } from "~/services/eventService";
+
+import EventBuyComponent from "~/components/events/EventBuyComponent.vue";
+import TicketTypeComponent from "~/components/ticketType/TicketTypeComponent.vue";
+import AlertComponent from "~/components/alerts/Alert1Component.vue";
+import PaymentPopup from "~/components/popups/PaymentPopup.vue";
 
 const selectedTickets = ref<
   {
@@ -104,7 +121,7 @@ function checkTicketTypes() {
   const noTicketsElement = document.getElementById("no-tickets");
   if (tickettypes.value.length == 0) {
     if (noTicketsElement) noTicketsElement.style.display = "block";
-  } else if (noTicketsElement) { 
+  } else if (noTicketsElement) {
     noTicketsElement.style.display = "none";
   }
 }
@@ -132,10 +149,10 @@ function getUserInputs() {
 function calculateAmount() {
   getUserInputs();
   amount.value = 0;
-  for (const ticket of selectedTickets.value) { 
+  for (const ticket of selectedTickets.value) {
     amount.value += ticket.price * ticket.quantity;
   }
-  amount.value /= 100; 
+  amount.value /= 100;
   return amount;
 }
 //new Alert/Fehler nachricht
@@ -177,6 +194,3 @@ function checkInputs() {
   return inputsAreNumeric;
 }
 </script>
-
-
-
--- Ticket-Shop/pages/index.vue
+++ Ticket-Shop/pages/index.vue
@@ -2,12 +2,17 @@
   <div class="mx-auto">
     <BannerComponent />
     <div class="flex justify-center items-center">
-      <p id="no-results" class="flex justify-center text-gray-600 my-10" style="display: none">
+      <p
+        id="no-results"
+        class="flex justify-center text-gray-600 my-10"
+        style="display: none"
+      >
         Keine Suchergebnisse
       </p>
     </div>
     <div
-      class="pt-4 px-4 hover:rounded-lg rounded-3xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-6 gap-4">
+      class="pt-4 px-4 hover:rounded-lg rounded-3xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-6 gap-4"
+    >
       <EventComponent v-for="event in events" :event="event"></EventComponent>
     </div>
   </div>
@@ -15,11 +20,12 @@
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
-import EventComponent from "~/components/Events/EventComponent.vue";
-import BannerComponent from "~/components/Alerts/BannerComponent.vue";
 import { Event } from "~/classes/Event";
 import { fetchEvents, fetchEventsByEventName } from "~/services/eventService";
 import { useSearchStore } from "~/stores/searchStore";
+
+import EventComponent from "~/components/events/EventComponent.vue";
+import BannerComponent from "~/components/alerts/BannerComponent.vue";
 
 const events = ref<Event[]>([]);
 const searchStore = useSearchStore();
@@ -46,7 +52,8 @@ async function loadEventsBasedOnName(eventName: string) {
 function updateNoResultsDisplay() {
   const noResultsElement = document.getElementById("no-results");
   if (noResultsElement) {
-    noResultsElement.style.display = events.value.length === 0 ? "block" : "none";
+    noResultsElement.style.display =
+      events.value.length === 0 ? "block" : "none";
   }
 }
 
